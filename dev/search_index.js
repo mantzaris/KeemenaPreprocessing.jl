998717dd0ea12f15baae5a316dbf82953b50f985
Documenter.jl <documenter@juliadocs.github.io>
2025-06-19 02:26:48 +0000

    build based on 2db3e6b

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"#KeemenaPreprocessing","page":"Home","title":"KeemenaPreprocessing","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for KeemenaPreprocessing.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"}]
+}
