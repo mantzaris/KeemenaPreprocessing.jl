e35af30f4f1822f393886a1230824775606d10e0
mantzaris <avmantzaris@gmail.com>
2025-06-24 20:23:12 -0400

    fixes the types.jl so that it is more extensible by subsequent subword processing steps

--- src/core/types.jl
+++ src/core/types.jl
@@ -1,65 +1,102 @@
 
-const Offset = Int
 
-struct Vocabulary{ID<:Unsigned}
-    id_to_token     :: Vector{String}
-    token_to_id     :: Dict{String,ID}
-    token_frequencies :: Vector{Int64}
-    specials        :: Dict{Symbol,ID}
+export Vocabulary, CorpusStorage, PipelineMetadata,
+       PreprocessBundle, haslevel, with_extras!,
+       DEFAULT_LEVELS
+
+
+struct Vocabulary{IdT<:Unsigned}
+    id_to_token_strings  :: Vector{String}
+    token_to_id_map      :: Dict{String,IdT}
+    token_frequencies    :: Vector{Int64}
+    special_tokens       :: Dict{Symbol,IdT}
 end
 
-struct CorpusStorage{ID<:Unsigned}
-    token_ids          :: Vector{ID} 
-    document_offsets   :: Vector{Offset}
-    sentence_offsets   :: Union{Vector{Offset},Nothing}
-    paragraph_offsets  :: Union{Vector{Offset},Nothing}
+
+struct CorpusStorage{IdT<:Unsigned, OffsetT<:Integer}
+    token_ids         :: Vector{IdT}          # flat corpus
+    document_offsets  :: Vector{OffsetT}           # length = D+1, 1-based, sentinel end
+    sentence_offsets  :: Union{Vector{OffsetT},Nothing}
+    paragraph_offsets :: Union{Vector{OffsetT},Nothing}
 end
+
 
 struct PipelineMetadata
     configuration :: Dict{Symbol,Any} #cleaning and tokeniser params
-    # any extras
 end
+PipelineMetadata() = PipelineMetadata(Dict{Symbol,Any}())
 
-struct ExtraArrays
-    token_length :: Union{Vector{UInt16},Nothing}
-    ngram_hash   :: Union{Vector{UInt64},Nothing}
+
+
+"""
+`PreprocessBundle{IdT,OffsetT,ExtraT}`
+
+  * `IdT`    : unsigned integer type for token ids (e.g. `UInt32`)
+  * `OffsetT`   : integer type for offsets  (e.g. `Int` or `UInt32`)
+  * `ExtraT` : payload supplied by downstream packages (`Nothing` by default)
+"""
+struct PreprocessBundle{IdT<:Unsigned, OffsetT<:Integer, ExtraT}
+    corpus_storage    :: CorpusStorage{IdT,OffsetT}
+    vocabulary        :: Vocabulary{IdT}
+    pipeline_metadata :: PipelineMetadata
+    extras            :: ExtraT
+    levels_present    :: Dict{Symbol,Bool}
 end
-
-struct PreprocessBundle{ID<:Unsigned}
-    corpus    :: CorpusStorage{ID}
-    vocabulary:: Vocabulary{ID}
-    metadata  :: PipelineMetadata
-    extras    :: Union{ExtraArrays,Nothing}
-    levels    :: Dict{Symbol,Bool}
-end
-
 
 
 ##############
 # Constructors
 
+
 const DEFAULT_LEVELS = Dict(
     :character => false, :word => false, :sentence => false,
     :paragraph => false, :document => false)
 
-function PreprocessBundle(corpus::CorpusStorage{ID},
-                          vocab :: Vocabulary{ID};
-                          metadata = PipelineMetadata(Dict()),
-                          extras   = nothing,
-                          levels   = DEFAULT_LEVELS) where {ID}
+    
+function PreprocessBundle(corpus_storage::CorpusStorage{IdT,OffsetT},
+                          vocabulary ::Vocabulary{IdT};
+                          pipeline_metadata = PipelineMetadata(),
+                          extras            = nothing,
+                          levels_present    = DEFAULT_LEVELS) where {IdT,OffsetT}
 
-    @assert corpus.document_offsets[end] == length(corpus.token_ids) + 1
-
-    if corpus.sentence_offsets !== nothing
-        @assert corpus.sentence_offsets[end] == length(corpus.token_ids) + 1
+    N = length(corpus_storage.token_ids)
+    @assert corpus_storage.document_offsets[end] == N + 1
+    if corpus_storage.sentence_offsets !== nothing
+        @assert corpus_storage.sentence_offsets[end] == N + 1
+    end
+    if corpus_storage.paragraph_offsets !== nothing
+        @assert corpus_storage.paragraph_offsets[end] == N + 1
     end
 
-    if corpus.paragraph_offsets !== nothing
-        @assert corpus.paragraph_offsets[end] == length(corpus.token_ids) + 1
-    end
-
-    PreprocessBundle{ID}(corpus, vocab, metadata, extras, copy(levels))
+    PreprocessBundle{IdT,OffsetT,typeof(extras)}(
+        corpus_storage, vocabulary, pipeline_metadata, extras, copy(levels_present))
 end
 
-haslevel(pb::PreprocessBundle, lvl::Symbol) =
-    get(pb.levels, lvl, false)
+
+"""
+    with_extras!(bundle, new_extras; setlevel = nothing) -> new_bundle
+
+Return a **new** `PreprocessBundle` sharing the same corpus & vocab but carrying
+`new_extras`.  If `setlevel` is provided it toggles the corresponding
+`levels_present` flag to `true`.
+"""
+function with_extras!(bundle::PreprocessBundle{IdT,OffsetT},
+                      new_extras;
+                      setlevel::Union{Symbol,Nothing}=nothing
+                      ) where {IdT,OffsetT}
+
+    new_levels = copy(bundle.levels_present)
+    if setlevel !== nothing
+        new_levels[setlevel] = true
+    end
+
+    return PreprocessBundle{IdT,OffsetT,typeof(new_extras)}(
+        bundle.corpus_storage,
+        bundle.vocabulary,
+        bundle.pipeline_metadata,
+        new_extras,
+        new_levels)
+end
+
+haslevel(pb::PreprocessBundle, level::Symbol) =
+    get(pb.levels_present, level, false)
