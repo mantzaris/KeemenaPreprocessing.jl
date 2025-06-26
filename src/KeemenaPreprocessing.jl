044ce0f703a2364a13b8e2cf790395f6a6587f64
mantzaris <avmantzaris@gmail.com>
2025-06-26 01:28:27 -0400

    now the structure of the directories and other files is in place

--- src/KeemenaPreprocessing.jl
+++ src/KeemenaPreprocessing.jl
@@ -1,6 +1,7 @@
 
 
 module KeemenaPreprocessing
+__precompile__()
 
 
 #core data types
@@ -8,6 +9,20 @@ include(joinpath(@__DIR__, "core", "types.jl"))
 include(joinpath(@__DIR__, "core", "configuration.jl"))
 
 
+#processing
+for f in ("cleaning.jl","segmentation.jl","tokenization.jl",
+          "vocabulary.jl","assemble.jl")
+    include(joinpath("processing", f))
+end
+
+
+#storage
+include("storage/readers.jl")
+include("storage/writers.jl")
+include("storage/preprocessor_state.jl")
+
+
+#public api facing
 include(joinpath(@__DIR__, "pipeline", "pipeline.jl"))
 
 
